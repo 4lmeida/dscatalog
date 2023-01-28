d44a854fcc1723b363d0c19176e9e7f3b637c6f4
Luis Almeida <lferreira646@gmail.com>
2023-01-28 18:41:46 -0300

    TestIT Get and Put

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIT.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIT.java
@@ -1,13 +1,18 @@
 package com.devsuperior.dscatalog.resources;
 
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.tests.Factory;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -18,9 +23,13 @@ public class ProductResourceIT {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     private Long existingId;
     private Long nonExistingId;
     private Long countTotalProducts;
+    private ProductDTO productDTO;
 
     @BeforeEach
     void setUp() {
@@ -28,15 +37,56 @@ public class ProductResourceIT {
         existingId = 1L;
         nonExistingId = 1000L;
         countTotalProducts = 25L;
-
+        productDTO = Factory.createProductDTO();
     }
 
     @Test
     public void findAllShouldReturnPagedSortedWhenSortByName() throws Exception {
 
-        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.get(""));
+        ResultActions result = mockMvc.perform(
+                MockMvcRequestBuilders.get("/products?page=0&size=12&sort=name,asc")
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(MockMvcResultMatchers.status().isOk());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.content").exists());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.totalElements").value(countTotalProducts));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.content[0].name").value("Macbook Pro"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.content[1].name").value("PC Gamer"));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.content[2].name").value("PC Gamer Alfa"));
 
     }
 
+    @Test
+    public void updateShouldReturnProductDTOWhenIdExists() throws Exception {
+
+        String expectedName = productDTO.getName();
+        String expectedDescription = productDTO.getDescription();
+
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.put("/products/{id}", existingId)
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(MockMvcResultMatchers.status().isOk());
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(existingId));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.name").value(expectedName));
+        result.andExpect(MockMvcResultMatchers.jsonPath("$.description").value(expectedDescription));
+    }
+
+    @Test
+    public void updateShouldReturnResourceNotFoundExceptionWhenIdDoesNotExist() throws Exception {
+
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.put("/products/{id}", nonExistingId)
+                .content(jsonBody)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
 
 }
