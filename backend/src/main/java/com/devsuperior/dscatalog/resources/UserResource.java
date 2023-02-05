1ae2f953db9ad631afd93fad50e9de9c2c794d75
Luis Almeida <lferreira646@gmail.com>
2023-02-05 10:51:47 -0300

    User CRUD

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
@@ -0,0 +1,56 @@
+package com.devsuperior.dscatalog.resources;
+
+import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@Controller
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping
+    public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto)  {
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        UserDTO userDTO = service.insert(dto);
+        return ResponseEntity.created(uri).body(userDTO);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
+        UserDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserDTO>> findAll(Pageable pageable) {
+        Page<UserDTO> page = service.findAll(pageable);
+        return ResponseEntity.ok().body(page);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody UserDTO dto) {
+        dto = service.update(id, dto);
+        return  ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
