2f91878fe21cf7338ea95f20ee09cfb5952fe4b5
Luis <lferreira646@gmail.com>
2023-06-27 18:05:18 -0300

    projeto limpo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,8 @@
-<<<<<<< HEAD
-import React from 'react';
-import logo from './logo.svg';
-=======
->>>>>>> b93b22b9a0000ba56489d22374933b2a9c34beae
 import './App.css';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
-    <h1>Hello World</h1>
->>>>>>> b93b22b9a0000ba56489d22374933b2a9c34beae
+    <h1>Hello mundo</h1>
   );
 }
 
